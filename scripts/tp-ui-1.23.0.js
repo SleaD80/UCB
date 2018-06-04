94e25d28c6c8454fa51e7fab3c915c2813ab2c4f
slead <slead@MacBook-Pro-SleaD.local>
2018-06-04 16:44:08 +0300

    Format code

--- scripts/tp-ui-1.23.0.js
+++ scripts/tp-ui-1.23.0.js
@@ -40,98 +40,96 @@ var person = {
         var props = Object.keys(person);
         data.substr(2).split('//',4).forEach(function(item, i) {
             typeof person[props[i]] === 'string'?person[props[i]]=item:person[props[i]].set(item);
-        })}
-};
+        })}};
 
 $(document).ready(function(){
     ECMA262();
 
     //region GetControls
-    var $details = $("#payment_details");
-    var $tpDetails = $("#tp_payment_details");
-    var $tpINN = $("#tp_inn");
-    var $tpName = $("#tp_name");
-    var $tpActivity = $("#tp_activity_kind");
-    var $tpAddr = $("#tp_address");
-    var $tpDN = $("#tp_docnumber");
-    var $tpDD = $("#tp_docdate");
-    var $tpPanel = $('#tpPaymentPanel');
-    var $tpCheckbox = $('#tpPaymentCheckbox');
+        var $details = $("#payment_details");
+        var $tpDetails = $("#tp_payment_details");
+        var $tpINN = $("#tp_inn");
+        var $tpName = $("#tp_name");
+        var $tpActivity = $("#tp_activity_kind");
+        var $tpAddr = $("#tp_address");
+        var $tpDN = $("#tp_docnumber");
+        var $tpDD = $("#tp_docdate");
+        var $tpPanel = $('#tpPaymentPanel');
+        var $tpCheckbox = $('#tpPaymentCheckbox');
     //endregion
 
     //region OverrideHandlers
-    function saveTP() {
-        var $details = $("#payment_details");
-        $details.val(person.print() + $details.val());
-    }
-    /*
-    var _saveDraftClick = saveDraftClick;
-    saveDraftClick = function() {
-        saveTP();
-        _saveDraftClick();
-    };
+        function saveTP() {
+            var $details = $("#payment_details");
+            $details.val(person.print() + $details.val());
+        }
+        /*
+        var _saveDraftClick = saveDraftClick;
+        saveDraftClick = function() {
+            saveTP();
+            _saveDraftClick();
+        };
 
-    var _printClick = printClick;
-    printClick = function() {
-        saveTP();
-        _printClick();
-    };
+        var _printClick = printClick;
+        printClick = function() {
+            saveTP();
+            _printClick();
+        };
 
-    var _onChangePaymentType = onChangePaymentType;
-    onChangePaymentType = function(e) {
-        $tpCheckbox.show();
-        _onChangePaymentType(e);
-    };
-    */
+        var _onChangePaymentType = onChangePaymentType;
+        onChangePaymentType = function(e) {
+            $tpCheckbox.show();
+            _onChangePaymentType(e);
+        };
+        */
     //endregion
 
     //region SetMasks
-    $.mask.definitions['1'] = '[01]';
-    $.mask.definitions['3'] = '[0123]';
-    $tpDD.mask("39.19.2999");
-    $tpINN.mask("99999?9999999",{placeholder: ""});
+        $.mask.definitions['1'] = '[01]';
+        $.mask.definitions['3'] = '[0123]';
+        $tpDD.mask("39.19.2999");
+        $tpINN.mask("99999?9999999",{placeholder: ""});
     //endregion
 
     //region SetListeners
-    $tpINN.focusout(function(){ check(); person._inn=$tpINN.val(); $tpDetails.val(person.print()) });
-    $tpName.focusout(function(){ check(); person._name._fio=$tpName.val(); $tpDetails.val(person.print()) });
-    $tpActivity.focusout(function(){ check(); person._name._activity=$tpActivity.val(); $tpDetails.val(person.print()) });
-    $tpAddr.focusout(function(){ check(); person._address = $tpAddr.val(); $tpDetails.val(person.print()) });
-    $tpDN.focusout(function(){ person._doc._number=$tpDN.val(); $tpDetails.val(person.print()) });
-    $tpDD.focusout(function(){ person._doc._date=$tpDD.val(); $tpDetails.val(person.print()) });
-    $tpCheckbox.click(function(){ $tpPanel.is(':visible')===true?$tpPanel.hide():$tpPanel.show() });
+        $tpINN.focusout(function(){ check(); person._inn=$tpINN.val(); $tpDetails.val(person.print()) });
+        $tpName.focusout(function(){ check(); person._name._fio=$tpName.val(); $tpDetails.val(person.print()) });
+        $tpActivity.focusout(function(){ check(); person._name._activity=$tpActivity.val(); $tpDetails.val(person.print()) });
+        $tpAddr.focusout(function(){ check(); person._address = $tpAddr.val(); $tpDetails.val(person.print()) });
+        $tpDN.focusout(function(){ person._doc._number=$tpDN.val(); $tpDetails.val(person.print()) });
+        $tpDD.focusout(function(){ person._doc._date=$tpDD.val(); $tpDetails.val(person.print()) });
+        $tpCheckbox.click(function(){ $tpPanel.is(':visible')===true?$tpPanel.hide():$tpPanel.show() });
     //endregion
 
     //region InitControls
-    person.init($details.val());
-    $tpDetails.val(person.print());
-    $tpINN.val(person._inn);
-    $tpName.val(person._name._fio);
-    $tpAddr.val(person._address);
-    $tpActivity.val(person._name._activity);
-    fillSelectOptions($tpActivity, activities);
-    $tpDN.val(person._doc._number);
-    $tpDD.val(person._doc._date);
+        person.init($details.val());
+        $tpDetails.val(person.print());
+        $tpINN.val(person._inn);
+        $tpName.val(person._name._fio);
+        $tpAddr.val(person._address);
+        $tpActivity.val(person._name._activity);
+        fillSelectOptions($tpActivity, activities);
+        $tpDN.val(person._doc._number);
+        $tpDD.val(person._doc._date);
 
-    //TODO $details.val() = NULL
-    //$details.val($details.val().substr($tpDetails.val().length));
-    person.print().length > 0?$tpPanel.show():$tpPanel.hide();
-    $('#payment_type').val()=="regular_payment"?$tpCheckbox.hide():$tpCheckbox.show();
-
+        //TODO $details.val() = NULL
+        //$details.val($details.val().substr($tpDetails.val().length));
+        person.print().length > 0?$tpPanel.show():$tpPanel.hide();
+        $('#payment_type').val()=="regular_payment"?$tpCheckbox.hide():$tpCheckbox.show();
     //endregion
 
     //region Checks
-    function renderField(control,err) {
-        setFieldStatus(control, err);
-        setFieldMessage(control, err.message);
-    }
+        function renderField(control,err) {
+            setFieldStatus(control, err);
+            setFieldMessage(control, err.message);
+        }
 
-    function check() {
-        renderField($tpINN,validateInn($tpINN.val()));
-        renderField($tpINN,checkRules(innRules($tpActivity.val(),$tpINN.val())));
-        renderField($tpName,checkRules(nameRules($tpActivity.val(),$tpName.val())));
-    }
+        function check() {
+            renderField($tpINN,validateInn($tpINN.val()));
+            renderField($tpINN,checkRules(innRules($tpActivity.val(),$tpINN.val())));
+            renderField($tpName,checkRules(nameRules($tpActivity.val(),$tpName.val())));
+        }
 
-    check();
+        check();
     //endregion
-});
+    });
